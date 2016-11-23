3d97c2f2c4f871e1cc022e79b4cda04fa1e2392b
york yao <yaoao12306@outlook.com>
2016-11-24 07:37:41 +0800

    use schema-based-json-editor rather than json-editor

--- scripts/index.tsx
+++ scripts/index.tsx
@@ -4,19 +4,18 @@ import * as electron from "electron";
 import * as React from "react";
 import * as types from "../types";
 import * as ReactDOM from "react-dom";
+import { JSONEditor } from "schema-based-json-editor/dist/react";
 
 (window as any)["jQuery"] = $;
 require("bootstrap");
-require("json-editor");
 
 const body = $("html,body");
-let editor: JSONEditor<types.ConfigData>;
 
 /* tslint:disable:only-arrow-functions */
-$(document).on("click", "a[href^='http']", function (this: HTMLAnchorElement, e: JQueryEventObject) {
+$(document).on("click", "a[href^='http']", function(this: HTMLAnchorElement, e: JQueryEventObject) {
     e.preventDefault();
     electron.shell.openExternal(this.href);
-}).on("click", "a[href^='#']", function (this: HTMLAnchorElement, e: JQueryEventObject) {
+}).on("click", "a[href^='#']", function(this: HTMLAnchorElement, e: JQueryEventObject) {
     e.preventDefault();
     e.stopPropagation();
     const top = $($(this).attr("href")).offset().top - 20;
@@ -32,68 +31,68 @@ type State = {
 };
 
 class MainComponent extends React.Component<{}, State> {
-    constructor() {
-        super();
-        this.state = {
-            news: [],
-            configurationDialogIsVisiable: false,
-        };
+    schema: any;
+    value: types.ConfigData;
+    locale = navigator.language;
+    isValid: boolean;
+    news: types.NewsCategory[] = [];
+    configurationDialogIsVisiable = false;
+
+    updateValue = (value: any, isValid: boolean) => {
+        this.value = value;
+        this.isValid = isValid;
     }
-    public reload(n: types.NewsCategory) {
+    reload(n: types.NewsCategory) {
         n.error = undefined;
-        this.setState({ news: this.state.news });
+        this.setState({ news: this.news });
         electron.ipcRenderer.send("reload", n.source);
     }
-    public hide(item: types.NewsItem) {
+    hide(item: types.NewsItem) {
         item.hidden = true;
-        this.setState({ news: this.state.news });
+        this.setState({ news: this.news });
         electron.ipcRenderer.send("hide", item.href);
     }
-    public openAndHide(item: types.NewsItem) {
+    openAndHide(item: types.NewsItem) {
         electron.shell.openExternal(item.href);
         this.hide(item);
     }
-    public componentDidMount() {
+    componentDidMount() {
         electron.ipcRenderer.on("items", (event: Electron.IpcRendererEvent, arg: types.NewsCategory) => {
-            const index = this.state.news!.findIndex(n => n.source === arg.source);
+            const index = this.news!.findIndex(n => n.source === arg.source);
             if (arg.name) {
                 arg.key = arg.name.replace(" ", "");
             }
             if (index === -1) {
-                this.state.news!.push(arg);
+                this.news!.push(arg);
             } else {
-                this.state.news![index] = arg;
+                this.news![index] = arg;
             }
-            this.setState({ news: this.state.news });
+            this.setState({ news: this.news });
         });
         electron.ipcRenderer.on("initialize", (event: Electron.IpcRendererEvent, arg: types.InitialData) => {
-            editor = new JSONEditor(document.getElementById("configuration") !, {
-                theme: "bootstrap3",
-                iconlib: "bootstrap3",
-                disable_edit_json: true,
-                disable_properties: true,
-                schema: arg.schema,
-                startval: arg.startval,
-            });
+            this.schema = arg.schema;
+            this.value = arg.startval;
         });
         electron.ipcRenderer.on("error", (event: Electron.IpcRendererEvent, arg: types.ErrorMessage) => {
             alert(arg.message);
         });
         electron.ipcRenderer.send("items");
     }
-    public toggleConfigurationDialog() {
+    toggleConfigurationDialog() {
+        this.configurationDialogIsVisiable = !this.configurationDialogIsVisiable;
         this.setState({
-            configurationDialogIsVisiable: !this.state.configurationDialogIsVisiable,
+            configurationDialogIsVisiable: this.configurationDialogIsVisiable,
         });
     }
-    public saveConfiguration() {
-        electron.ipcRenderer.send("saveConfiguration", editor.getValue() as types.ConfigData);
+    saveConfiguration() {
+        electron.ipcRenderer.send("saveConfiguration", this.value);
+        this.configurationDialogIsVisiable = false;
         this.setState({
-            configurationDialogIsVisiable: false,
+            configurationDialogIsVisiable: this.configurationDialogIsVisiable,
         });
     }
-    public render() {
-        const newsView = this.state.news!.map(n => {
+    render() {
+        const newsView = this.news!.map(n => {
             if (n.items) {
                 const itemsView = n.items.map((i, index) => {
                     let detailView: JSX.Element | undefined = undefined;
@@ -137,7 +136,7 @@ class MainComponent extends React.Component<{}, State> {
                 return (
                     <div key={n.source} className="panel panel-default">
                         <div className="panel-heading">
-                            <h3 className="panel-title"id={n.key}>
+                            <h3 className="panel-title" id={n.key}>
                                 <a href={n.source} className="btn btn-link">{n.name}</a>
                                 <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
                                 <span>{n.error}</span>
@@ -149,7 +148,7 @@ class MainComponent extends React.Component<{}, State> {
             }
         });
 
-        const menuView = this.state.news!.map(n => {
+        const menuView = this.news!.map(n => {
             let errorView: JSX.Element | undefined = undefined;
             if (!n.items) {
                 errorView = (
@@ -166,12 +165,25 @@ class MainComponent extends React.Component<{}, State> {
             );
         });
 
+        let configurationView: JSX.Element | null = null;
+        if (this.schema && this.configurationDialogIsVisiable) {
+            configurationView = (
+                <div id="configuration" className="configuration bootstrap3-row-container">
+                    <JSONEditor schema={this.schema}
+                        initialValue={this.value}
+                        updateValue={this.updateValue}
+                        theme="bootstrap3"
+                        icon="bootstrap3"
+                        locale={this.locale} />
+                </div>
+            );
+        }
+
         return (
             <div className="container">
                 <button className="btn btn-xs menu-configuration" onClick={this.toggleConfigurationDialog.bind(this)}>配置</button>
-                <button className={this.state.configurationDialogIsVisiable ? "btn btn-xs btn-primary menu-save" : "btn btn-xs menu-save btn-primary hidden"} onClick={this.saveConfiguration.bind(this)}>保存</button>
-                <div id="configuration" className={this.state.configurationDialogIsVisiable ? "configuration" : "configuration hidden"}>
-                </div>
+                <button className={this.configurationDialogIsVisiable ? "btn btn-xs btn-primary menu-save" : "btn btn-xs menu-save btn-primary hidden"} onClick={this.saveConfiguration.bind(this)}>保存</button>
+                {configurationView}
                 <ul className="menu list-unstyled">{menuView}</ul>
                 <div>{newsView}</div>
             </div>
