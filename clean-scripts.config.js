e79eb2b4ecdb90090354988a26ec73f1c2425bba
york yao <yaoao12306@outlook.com>
2017-07-28 00:24:32 +0800

    clean scripts

---
+++ clean-scripts.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  build: [
+    `tsc`,
+    `tsc -p static`,
+    `rimraf static/index.bundle.* static/css static/fonts`,
+    `cpy ./node_modules/bootstrap/dist/css/bootstrap.min.css static/css/`,
+    `cpy ./node_modules/bootstrap/fonts/*.* ./static/fonts`,
+    `cpy ./node_modules/highlight.js/styles/github.css static/css`,
+    `cleancss -o static/index.bundle.css static/index.css`,
+    `webpack --config static/webpack.config.js`
+  ],
+  lint: [
+    `tslint "scripts/*.tsx" "*.ts"`,
+    `standard "**/*.config.js"`
+  ],
+  test: [
+    'tsc -p spec',
+    'jasmine',
+    'tsc -p static_spec',
+    'karma start static_spec/karma.config.js'
+  ],
+  fix: [
+    `standard --fix "**/*.config.js"`
+  ],
+  release: [
+    `rimraf dist`,
+    `clean-release`
+  ]
+}
