39e4f44d62ba2b539543c9fec294c77362588882
york yao <yaoao12306@outlook.com>
2017-08-07 23:20:25 +0800

    update webpack config; improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,27 +1,33 @@
 module.exports = {
-  build: [
-    `tsc`,
-    `tsc -p static`,
-    `rimraf static/index.bundle.* static/css static/fonts`,
-    `cpy ./node_modules/bootstrap/dist/css/bootstrap.min.css static/css/`,
-    `cpy ./node_modules/bootstrap/fonts/*.* ./static/fonts`,
-    `cpy ./node_modules/highlight.js/styles/github.css static/css`,
-    `cleancss -o static/index.bundle.css static/index.css`,
-    `webpack --display-modules --config static/webpack.config.js`
-  ],
-  lint: [
-    `tslint "scripts/*.tsx" "*.ts"`,
-    `standard "**/*.config.js"`
-  ],
-  test: [
-    'tsc -p spec',
-    'jasmine',
-    'tsc -p static_spec',
-    'karma start static_spec/karma.config.js'
-  ],
-  fix: [
-    `standard --fix "**/*.config.js"`
-  ],
+  build: {
+    back: `tsc`,
+    js: [
+      `tsc -p static`,
+      `webpack --display-modules --config static/webpack.config.js`
+    ],
+    css: `cleancss -o static/index.bundle.css static/index.css`,
+    copy: [
+      `rimraf static/css static/fonts`,
+      `cpy ./node_modules/bootstrap/dist/css/bootstrap.min.css static/css/`,
+      `cpy ./node_modules/bootstrap/fonts/*.* ./static/fonts`,
+      `cpy ./node_modules/highlight.js/styles/github.css static/css`
+    ]
+  },
+  lint: {
+    ts: `tslint "scripts/*.tsx" "*.ts"`,
+    js: `standard "**/*.config.js"`
+  },
+  test: {
+    jasmine: [
+      'tsc -p spec',
+      'jasmine'
+    ],
+    karma: [
+      'tsc -p static_spec',
+      'karma start static_spec/karma.config.js'
+    ]
+  },
+  fix: `standard --fix "**/*.config.js"`,
   release: [
     `rimraf dist`,
     `clean-release`
